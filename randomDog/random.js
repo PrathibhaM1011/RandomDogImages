900e4b0c20608e78007b83e28b6d28d07fb68eb5
PrathibhaM1011 <prathibhamamindla2910@gmail.com>
2025-03-21 11:56:37 +0530

    Update random.js

--- randomDog/random.js
+++ randomDog/random.js
@@ -1,74 +1,58 @@
-// let container = document.getElementsByClassName("container")[0];
-// async function getData(){
-//     try{
-//         let res = await fetch("https://dog.ceo/api/breeds/image/random");
-//         if(!res.ok){
-//             throw new Error("HTTP error!", res.status);
-//         }
-//         let obj = await res.json()
-//         let { message, status} = obj;
-//         displayData(message, status)
-//     }
-//     catch(err){
-//         console.warn(err)
-//     }
-// }
-// function displayData(url, status1){
-//     container.innerHTML = `
-//     <img src='${url}' style="height:200px; width:200px; border: 1px solid gray">
-//     <p> Status : ${status1}</p>
-//     `;
-// }
+document.addEventListener("DOMContentLoaded", function () {
+    let container = document.getElementById("container");
+    let loader = document.getElementById("loader");
 
+    async function getData() {
+        try {
+            let res = await fetch("https://dog.ceo/api/breeds/image/random");
+            if (!res.ok) {
+                throw new Error("HTTP error! Status: " + res.status);
+            }
+            let obj = await res.json();
+            let { message, status } = obj;
 
-// window.addEventListener("DOMContentLoaded", function(){
-//     getData();
-// })
+            setTimeout(() => {
+                displayData(message, status);
+                saveData(message, status);
+            }, 4000); // 4 seconds delay
 
-let container = document.getElementsByClassName("container")[0];
-
-async function getData() {
-    try {
-        let res = await fetch("https://dog.ceo/api/breeds/image/random");
-        if (!res.ok) {
-            throw new Error("HTTP error! Status: " + res.status);
+        } catch (err) {
+            console.error("Error:", err.message);
         }
-        let obj = await res.json();
-        let { message, status } = obj;
-
-        displayData(message, status);
-        saveData(message, status);  // Send data to db.json
-    } catch (err) {
-        console.warn(err);
     }
-}
 
-function displayData(url, status1) {
-    container.innerHTML = `
-    <img src='${url}' style="height:200px; width:200px; border: 1px solid gray">
-    <p> Status : ${status1}</p>
-    `;
-}
-
-async function saveData(url, status1) {
-    try {
-        let response = await fetch("http://localhost:3000/dogs", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ image: url, status: status1 })
-        });
-
-        if (!response.ok) {
-            throw new Error("Failed to save data");
+    function displayData(url, status1) {
+        if (!container) {
+            console.error("Error: container not found!");
+            return;
         }
-        console.log("Data saved successfully!");
-    } catch (error) {
-        console.error("Error:", error);
-    }
-}
+        loader.remove(); // Hide loader
+        container.style.display = "block"; // Show container
 
-window.addEventListener("DOMContentLoaded", function () {
-    getData();
+        container.innerHTML = `
+            <img src="${url}" alt="Random Dog">
+            <p>Status: ${status1}</p>
+        `;
+    }
+
+    async function saveData(url, status1) {
+        try {
+            let response = await fetch("http://localhost:3000/dogs", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ image: url, status: status1 })
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to save data");
+            }
+            console.log("Data saved successfully!");
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    }
+
+    getData(); // Call function only after DOM is loaded
 });
